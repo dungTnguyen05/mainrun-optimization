8486182bdf016dad2e63712c2f64ce733c499f37
Tri Dung Nguyen <tridung.190705@gmail.com>
2026-07-11 12:09:59 +0000

    Optimization 21: Tune warmup ratio to 0.15

--- mainrun/train.py
+++ mainrun/train.py
@@ -81,7 +81,10 @@ class Hyperparameters:
     # warmup_ratio: float = 0.1
 
     # Optimization 20: Tune warmup ratio to 0.12
-    warmup_ratio: float = 0.12
+    # warmup_ratio: float = 0.12
+
+    # Optimization 21: Tune warmup ratio to 0.15
+    warmup_ratio: float = 0.15
 
     evals_per_epoch: int = 3
     
